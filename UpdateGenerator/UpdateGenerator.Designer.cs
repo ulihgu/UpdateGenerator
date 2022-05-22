dd25c931fa3a5eaacafb4162a88dff8264f1a280
ulihgu <ulihgu@msn.ocm>
2022-05-22 16:33:08 +0800

    添加项目文件。

---
+++ UpdateGenerator/UpdateGenerator.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace UpdateGenerator
+{
+    partial class UpdateGenerator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateGenerator));
+            this.progressBar1 = new MyProgressBar();
+            this._ButPath = new System.Windows.Forms.Button();
+            this._Path = new System.Windows.Forms.TextBox();
+            this._generatedFile = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1, 273);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(550, 23);
+            this.progressBar1.TabIndex = 27;
+            // 
+            // _ButPath
+            // 
+            this._ButPath.BackColor = System.Drawing.Color.SteelBlue;
+            this._ButPath.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this._ButPath.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this._ButPath.ForeColor = System.Drawing.Color.Transparent;
+            this._ButPath.Location = new System.Drawing.Point(333, 4);
+            this._ButPath.Name = "_ButPath";
+            this._ButPath.Size = new System.Drawing.Size(103, 23);
+            this._ButPath.TabIndex = 12;
+            this._ButPath.Text = "选择程序目录";
+            this._ButPath.UseVisualStyleBackColor = false;
+            this._ButPath.Click += new System.EventHandler(this._ButPath_Click);
+            // 
+            // _Path
+            // 
+            this._Path.Location = new System.Drawing.Point(2, 3);
+            this._Path.Name = "_Path";
+            this._Path.Size = new System.Drawing.Size(325, 21);
+            this._Path.TabIndex = 24;
+            // 
+            // _generatedFile
+            // 
+            this._generatedFile.BackColor = System.Drawing.Color.SteelBlue;
+            this._generatedFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this._generatedFile.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this._generatedFile.ForeColor = System.Drawing.Color.Transparent;
+            this._generatedFile.Location = new System.Drawing.Point(440, 4);
+            this._generatedFile.Name = "_generatedFile";
+            this._generatedFile.Size = new System.Drawing.Size(109, 23);
+            this._generatedFile.TabIndex = 25;
+            this._generatedFile.Text = "生成配置文件";
+            this._generatedFile.UseVisualStyleBackColor = false;
+            this._generatedFile.Click += new System.EventHandler(this._generatedFile_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.BackColor = System.Drawing.SystemColors.Control;
+            this.tbLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLog.Location = new System.Drawing.Point(2, 30);
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(550, 239);
+            this.tbLog.TabIndex = 26;
+            this.tbLog.Text = "";
+            // 
+            // UpdateGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(552, 297);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this._generatedFile);
+            this.Controls.Add(this._Path);
+            this.Controls.Add(this._ButPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "UpdateGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "更新配置文件生成器v2.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _ButPath;
+        private System.Windows.Forms.TextBox _Path;
+        private System.Windows.Forms.Button _generatedFile;
+        private System.Windows.Forms.RichTextBox tbLog;
+        //private System.Windows.Forms.ProgressBar progressBar1;
+        private MyProgressBar progressBar1;
+    }
+}
+
---
+++ UpdateGenerator/UpdateGenerator.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace UpdateGenerator
+{
+    public partial class UpdateGenerator : Form
+    {
+        /// <summary>
+        /// FileHelper 中, 文件MD5计算错误时 的默认值
+        /// </summary>
+        public const string ERROR_FILE_MD5 = "XXXXXXXXXXXXXXXXXXXX";
+        XMLProcess updateXml = new XMLProcess("update.xml");
+        Color COLORDEFAULT = Color.Black;				// default color in text-output panel
+        Color COLORCOMMAND = Color.DarkGreen;			// ftp command color
+        Color COLORERROR = Color.Red;				// color of error messages	
+        int count = 0;//文件总数量,当前XML文件段
+        int fileNum = 0;//文件总数量
+        int root = 0;//根目录位置
+        BackgroundWorker _XmlgroundWorker = new BackgroundWorker();
+        public UpdateGenerator()
+        {
+            InitializeComponent();
+            _XmlgroundWorker = new BackgroundWorker();
+            _XmlgroundWorker.WorkerSupportsCancellation = true;
+            _XmlgroundWorker.DoWork += _XmlgroundWorkerOnDoWork;
+            _XmlgroundWorker.RunWorkerCompleted += _XmlgroundWorkerComplete;
+        }
+        private void _XmlgroundWorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                if (GetFileNum(_Path.Text) != 0)
+                {
+                    count = 0;
+                    fileNum = 0;
+                    progressBar1.Invoke((MethodInvoker)delegate
+                      {
+                          progressBar1.Maximum = GetFileNum(_Path.Text);
+                          progressBar1.Value = 0;
+                          GetDirectoryFileToXML(_Path.Text);
+                          // WriteToLog("生成XML升级文档完成！", COLORCOMMAND);
+                      });
+                }
+                else
+                {
+                    tbLog.Invoke((MethodInvoker)delegate
+                    {
+                        WriteToLog("空的文件夹，请重新选择！", COLORERROR);
+                    });
+                }
+            }
+            catch (Exception err)
+            {
+                tbLog.Invoke((MethodInvoker)delegate
+                {
+                    WriteToLog("空的文件夹，请重新选择！", COLORERROR);
+                });
+                //return;
+            }
+        }
+        private void _XmlgroundWorkerComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            count = 0;//文件总数量,当前XML文件段
+            fileNum = 0;//文件总数量
+            root = 0;//根目录位置
+            WriteToLog("生成XML文档完成：" + _Path.Text + "\\update.xml", COLORCOMMAND);
+        }
+        public void WriteToLog(string message)
+        {
+            WriteToLog(message, COLORDEFAULT, true);
+        }
+
+        public void WriteToLog(string message, Color color)
+        {
+            WriteToLog(message, color, true);
+        }
+
+        public void WriteToLog(string message, bool crlf)
+        {
+            WriteToLog(message, COLORDEFAULT, crlf);
+        }
+
+        public void WriteToLog(string message, Color color, bool crlf)
+        {
+            tbLog.Focus();
+            tbLog.AppendText("");
+            tbLog.SelectionColor = color;
+            tbLog.AppendText(message + (crlf ? "\r\n" : null));
+        }
+        /// <summary>
+        /// 计算文件的MD5, 计算错误将返回 XXXXXXXXXXXXXXXXXXXX
+        /// </summary>
+        public static string GetFileMD5(string path)
+        {
+            if (!File.Exists(path)) return ERROR_FILE_MD5;
+
+            try
+            {
+                using (FileStream myFs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using (MD5 md5 = new MD5CryptoServiceProvider())
+                    {
+                        byte[] hash = md5.ComputeHash(myFs);
+                        myFs.Close();
+
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 0; i < hash.Length; i++) sb.Append(hash[i].ToString("x2"));
+                        return sb.ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return ERROR_FILE_MD5;
+            }
+        }
+        private void _ButPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (GetFileNum(dialog.SelectedPath) == 0)
+                    {
+                        tbLog.Clear();
+                        WriteToLog("空的文件夹，请重新选择！", COLORERROR);
+                        return;
+                    }
+                    _Path.Text = dialog.SelectedPath;
+                    root = _Path.Text.IndexOf(_Path.Text.Substring(_Path.Text.LastIndexOf(@"\"))) + 1;
+                    WriteToLog("文件夹正常，准备就绪！", COLORCOMMAND);
+                }
+            }
+            catch (Exception)
+            {
+                WriteToLog("空的文件夹，请重新选择！", COLORERROR);
+            }
+        }
+        /// <summary> 获取所有目录中文件，生成XML
+        /// </summary>
+        /// <param name="src">文件路径</param>
+        public long GetDirectoryFileToXML(String dirPath)
+        {
+
+            long len = 0;
+            string fileName;
+            string filePath;
+            //判断该路径是否存在（是否为文件夹）
+            try
+            {
+                if (!Directory.Exists(dirPath))
+                {
+                    //查询文件的大小
+                    //len = FileSize(dirPath);
+                    MessageBox.Show("请选择正确的目录！");
+                    return 0;
+                }
+                else
+                {
+                    //定义一个DirectoryInfo对象
+                    DirectoryInfo di = new DirectoryInfo(dirPath);
+
+                    //通过GetFiles方法，获取di目录中的所有文件的大小
+                    string fileVer = "0.0.0.0";
+                    foreach (FileInfo fi in di.GetFiles())
+                    {
+                        len += fi.Length;
+                        fileName = fi.Name;
+                        count = count + 1;
+                        filePath = fi.DirectoryName;
+                        //MessageBox.Show(fi.FullName);
+                        System.Diagnostics.FileVersionInfo info = System.Diagnostics.FileVersionInfo.GetVersionInfo(fi.FullName);
+                        //MessageBox.Show("文件1：" + i + "最大:" + filePath.Length);
+                        fileVer = "v"+info.FileVersion;
+                        if (fileVer.Trim()==null||fileVer.Trim()==""||fileVer=="v")
+                        {
+                            fileVer = "0.0.0.0";
+                        }
+                        else
+                        {
+                            fileVer = info.FileVersion;
+                        }
+                        progressBar1.Invoke((MethodInvoker)delegate
+                        {
+                            WriteToLog(count + "_文件名称:" + fileName, COLORCOMMAND);
+                            //MessageBox.Show("目录1：" + filePath);
+                            WriteToLog(count + "_所在目录：" + filePath, COLORERROR);
+                            progressBar1.Value = progressBar1.Value + 1;
+                            //生成配置文件开始  info.FileVersion    
+                        });
+                        //MessageBox.Show(info.FileVersion);
+                        XMLProcess.Insert("update.xml", "updateinformations", "FileCount_" + count, "", "");
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "FileName", "", fileName);
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "FilePath", "", filePath.Substring(root, filePath.Length - root));
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "FileVersion", "", fileVer);
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "FileSize", "", fi.Length.ToString());
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "CreationTime", "", fi.CreationTime.ToString());
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "ModificationTime", "", fi.LastWriteTime.ToString());
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "AccessTime", "", fi.LastAccessTime.ToString());
+                        XMLProcess.Insert("update.xml", "/updateinformations/FileCount_" + count, "FileInformation", "", GetFileMD5(filePath + "\\" + fileName));
+                    }
+                    //获取dir中所有的文件夹，并存到一个新的对象数组中，以进行递归
+                    DirectoryInfo[] dis = di.GetDirectories();
+                    if (dis.Length > 0)
+                    {
+                        for (int i = 0; i < dis.Length; i++)
+                        {
+                            len += GetDirectoryFileToXML(dis[i].FullName);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("取文件写入XML文件失败：" + err);
+            }
+            return len;
+        }
+        private void _generatedFile_Click(object sender, EventArgs e)
+        {
+            if (_Path.Text == "")
+            {
+                WriteToLog("空的文件夹，请重新选择！", COLORERROR);
+                return;
+            }
+            try
+            {
+                tbLog.Clear();
+                if (File.Exists("update.xml"))
+                    File.Delete("update.xml");
+
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlNode node = xmlDoc.CreateXmlDeclaration("1.0", "GB2312", null);
+                xmlDoc.AppendChild(node);
+                //加入根元素
+                XmlElement root = xmlDoc.CreateElement("updateinformations");
+                xmlDoc.AppendChild(root);
+                xmlDoc.Save("update.xml");
+
+                _XmlgroundWorker.RunWorkerAsync();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("执行清除并执行线程失败：" + err);
+            }
+            /*
+            XMLProcess.Insert("update.xml", "updateinformations", "b.txt", "", "");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "FilePath", "", "d:\\");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "FileSize", "", "1024");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "CreationTime", "", "11:11:11");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "ModificationTime", "", "12:12:12");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "AccessTime", "", "13:13:13");
+            XMLProcess.Insert("update.xml", "/updateinformations/b.txt", "MD5", "", "");
+             */
+
+        }
+        /// <summary>获取某目录下的所有文件(包括子目录下文件)的数量
+        /// </summary>
+        /// <param name="srcPath">目录</param>
+        public int GetFileNum(string srcPath)
+        {
+            try
+            {
+                //MessageBox.Show("GetFileNum-TRA位置：" + srcPath);
+                // if (srcPath.Length>0)
+                // {
+                string[] fileList = System.IO.Directory.GetFileSystemEntries(srcPath);
+                foreach (string file in fileList)
+                {
+                    if (System.IO.Directory.Exists(file))
+                        GetFileNum(file);
+                    else
+                        fileNum++;
+                    //  }
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("读取文件数量失败：" + e.ToString());
+            }
+            return fileNum;
+        }
+
+    }
+    class MyProgressBar : ProgressBar //新建一个MyProgressBar类，它继承了ProgressBar的所有属性与方法
+    {
+        public MyProgressBar()
+        {
+            base.SetStyle(ControlStyles.UserPaint, true);//使控件可由用户自由重绘
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            try
+            {
+                SolidBrush brush = null;
+                Rectangle bounds = new Rectangle(0, 0, base.Width, base.Height);
+                e.Graphics.FillRectangle(new SolidBrush(this.BackColor), 1, 1, bounds.Width - 2, bounds.Height - 2);//此处完成背景重绘，并且按照属性中的BackColor设置背景色
+                bounds.Height -= 4;
+                bounds.Width = ((int)(bounds.Width * (((double)base.Value) / ((double)base.Maximum)))) - 4;//是的进度条跟着ProgressBar.Value值变化
+                brush = new SolidBrush(this.ForeColor);
+                e.Graphics.FillRectangle(brush, 2, 2, bounds.Width, bounds.Height);//此处完成前景重绘，依旧按照Progressbar的属性设置前景色
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("空的文件夹：" + err);
+            }
+        }
+    }
+}
